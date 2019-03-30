dd29561674da3322b0f271a85b1d934a63cb5958
huskyhoochu <dfg1499@gmail.com>
2019-03-30 18:45:48 +0900

    Add pretter & git hook process

--- src/index.js
+++ src/index.js
@@ -1,64 +1,67 @@
-const fs = require('fs');
-
+const fs = require("fs");
 
 const cli = {
   execute() {
-    fs.open('./unicode.txt', 'w', (err, fd) => {
-      if(err) throw err;
+    fs.open("./unicode.txt", "w", (err, fd) => {
+      if (err) throw err;
       // 완성형
       let completeCount = 0;
       for (let x = 172; x < 216; x++) {
         for (let i = 0; i < 256; i++) {
-          let converted = '';
+          let converted = "";
           if (i < 16) {
-            converted = String.fromCharCode(`0X${x.toString(16)}0${i.toString(16)}`);
+            converted = String.fromCharCode(
+              `0X${x.toString(16)}0${i.toString(16)}`
+            );
           } else {
-            converted = String.fromCharCode(`0X${x.toString(16)}${i.toString(16)}`);
+            converted = String.fromCharCode(
+              `0X${x.toString(16)}${i.toString(16)}`
+            );
           }
-          
+
           if (completeCount >= 11172) {
             break;
           }
-    
+
           completeCount++;
           console.log(converted, completeCount);
-    
+
           let buf = new Buffer.from(converted);
           fs.write(fd, buf, 0, buf.length, null, (err, written, buffer) => {
             if (err) throw err;
-          })
+          });
         }
       }
-    
+
       // 자모
       let jamoCount = 0;
       for (let y = 49; y < 100; y++) {
-        let converted = '';
+        let converted = "";
         converted = String.fromCharCode(`0X31${y.toString(16)}`);
         jamoCount++;
         console.log(converted, jamoCount);
         let buf = new Buffer.from(converted);
         fs.write(fd, buf, 0, buf.length, null, (err, written, buffer) => {
           if (err) throw err;
-        })
+        });
       }
-    
+
       // ascii
       let asciiCount = 0;
       for (let a = 33; a < 127; a++) {
-        let converted = '';
+        let converted = "";
         converted = String.fromCharCode(a);
         asciiCount++;
         console.log(converted, asciiCount);
         let buf = new Buffer.from(converted);
         fs.write(fd, buf, 0, buf.length, null, (err, written, buffer) => {
           if (err) throw err;
-        })
+        });
       }
-    
-      fs.close(fd, () => console.log(`All process is done.`))
-    })    
+
+      fs.close(fd, () => console.log(`All process is done.`));
+    });
   }
-}
+};
 
 module.exports = cli;
