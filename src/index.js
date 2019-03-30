e99ea77605f09f9226c54acec2a059aa7058db2c
huskyhoochu <dfg1499@gmail.com>
2019-03-30 18:58:43 +0900

    Setup prettier

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-const fs = require("fs");
+'use strict';
+
+const fs = require('fs');
 
 const cli = {
   execute() {
-    fs.open("./unicode.txt", "w", (err, fd) => {
+    fs.open('./unicode.txt', 'w', (err, fd) => {
       if (err) throw err;
       // 완성형
       let completeCount = 0;
       for (let x = 172; x < 216; x++) {
         for (let i = 0; i < 256; i++) {
-          let converted = "";
+          let converted = '';
           if (i < 16) {
             converted = String.fromCharCode(
-              `0X${x.toString(16)}0${i.toString(16)}`
+              `0X${x.toString(16)}0${i.toString(16)}`,
             );
           } else {
             converted = String.fromCharCode(
-              `0X${x.toString(16)}${i.toString(16)}`
+              `0X${x.toString(16)}${i.toString(16)}`,
             );
           }
 
@@ -36,7 +38,7 @@ const cli = {
       // 자모
       let jamoCount = 0;
       for (let y = 49; y < 100; y++) {
-        let converted = "";
+        let converted = '';
         converted = String.fromCharCode(`0X31${y.toString(16)}`);
         jamoCount++;
         console.log(converted, jamoCount);
@@ -49,7 +51,7 @@ const cli = {
       // ascii
       let asciiCount = 0;
       for (let a = 33; a < 127; a++) {
-        let converted = "";
+        let converted = '';
         converted = String.fromCharCode(a);
         asciiCount++;
         console.log(converted, asciiCount);
@@ -61,7 +63,7 @@ const cli = {
 
       fs.close(fd, () => console.log(`All process is done.`));
     });
-  }
+  },
 };
 
 module.exports = cli;
