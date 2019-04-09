4e00c20298da59808d491fe9233d0f09e3e13f37
huskyhoochu <dfg1499@gmail.com>
2019-04-09 09:14:32 +0900

    Configure commaner process

--- bin/uni-bento.js
+++ bin/uni-bento.js
@@ -2,15 +2,26 @@
 
 'use strict';
 
-const bento = require('../lib/index');
+const program = require('commander');
+const moduleInfo = require('../package.json');
 
-const korean = process.argv.indexOf('--korean') > -1;
-const ascii = process.argv.indexOf('--ascii') > -1;
+program
+  .version(moduleInfo.version)
+  .description('[uni-bento] 🍱 A universal unicode table printer')
+  .option('--korean', 'Output all Korean characters from a unicode table')
+  .option('--ascii', 'Output all ASCII characters');
 
-if (korean) {
-  process.exitCode = bento.executeKorean();
-} else if (ascii) {
-  process.exitCode = bento.executeAscii();
-} else {
-  process.exitCode = bento.printHelp();
-}
+program.parse(process.argv);
+
+// const bento = require('../lib/index');
+
+// const korean = process.argv.indexOf('--korean') > -1;
+// const ascii = process.argv.indexOf('--ascii') > -1;
+
+// if (korean) {
+//   process.exitCode = bento.executeKorean();
+// } else if (ascii) {
+//   process.exitCode = bento.executeAscii();
+// } else {
+//   process.exitCode = bento.printHelp();
+// }
